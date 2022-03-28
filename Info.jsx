e57488dfc3bbe714337d279ef097294a33d70913
Jacob <37478093+Jacob12138xieyuan@users.noreply.github.com>
2022-03-28 10:32:30 +0800

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentNodeIndex: 0 };
+    this.previous = this.previous.bind(this);
+    this.next = this.next.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), this.props.delay);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    this.setState(state => ({ currentNodeIndex: state.currentNodeIndex + 1 }));
+  }
+
+  previous() {
+    this.setState(state => ({ currentNodeIndex: state.currentNodeIndex - 1 }));
+    clearInterval(this.interval);
+    this.interval = setInterval(() => this.tick(), this.props.delay);
+    console.log(
+      this.state.currentNodeIndex,
+      ((this.state.currentNodeIndex % this.props.children.length) + this.props.children.length) %
+        this.props.children.length,
+    );
+  }
+
+  next() {
+    this.setState(state => ({ currentNodeIndex: state.currentNodeIndex + 1 }));
+    clearInterval(this.interval);
+    this.interval = setInterval(() => this.tick(), this.props.delay);
+  }
+
+  render() {
+    return (
+      <div className="carousel">
+        <div className="current">
+          {
+            this.props.children[
+              ((this.state.currentNodeIndex % this.props.children.length) +
+                this.props.children.length) %
+                this.props.children.length
+            ]
+          }
+        </div>
+        <div className="buttons">
+          <button className="button-previous" type="button" onClick={this.previous}>
+            Previous
+          </button>
+          <button className="button-next" type="button" onClick={this.next}>
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+Info.propTypes = {
+  delay: PropTypes.number.isRequired,
+  children: PropTypes.array.isRequired,
+};
+
+export default Info;
